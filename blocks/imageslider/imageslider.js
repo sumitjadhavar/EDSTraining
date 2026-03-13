31b7a0ee06d8b4dfe297063d8a4f6e078ab87d9f
SUMIT BAJIRAO JADHAVAR <kumarrohan528@gmail.com>
2026-03-13 13:46:20 +0530

    Update imageslider.js

--- blocks/imageslider/imageslider.js
+++ blocks/imageslider/imageslider.js
@@ -3,7 +3,7 @@ export default function decorate(block) {
   const container = block.children[0];
   if (!container) return;
  
-  /* main class */
+  
   container.classList.add("nav-head");
  
   /* create track */
