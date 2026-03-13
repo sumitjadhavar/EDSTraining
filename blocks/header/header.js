bf9c9eb463eb2a3dc28fb141823159329d1dbcb4
SUMIT BAJIRAO JADHAVAR <kumarrohan528@gmail.com>
2026-03-13 11:59:51 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -124,7 +124,7 @@ export default async function decorate(block) {
   nav.id = 'nav';
   while (fragment.firstElementChild) nav.append(fragment.firstElementChild);
 
-  const classes = ['brand', 'sections', 'tools'];
+  const classes = ['cart-bar','social-bar','brand', 'sections', 'tools'];
   classes.forEach((c, i) => {
     const section = nav.children[i];
     if (section) section.classList.add(`nav-${c}`);
